331f1ae8c0b1baa016c3250c202d3776575abf89
JorgeGallegoG <jorgallegog@gmail.com>
2020-07-30 23:04:42 +0200

    Improve info prints

--- main.py
+++ main.py
@@ -33,17 +33,17 @@ class InstaBot:
             self.driver.find_element_by_xpath("//a[contains(@href, 'following')]")\
                 .click()
             following = self._get_names()
-            print("**** Created list of following ****")
+            print("**** Created list of following ****", flush=True)
             self.driver.find_element_by_xpath("//a[contains(@href, 'followers')]")\
                 .click()
             followers = self._get_names()
-            print("**** Created list of following ****")
+            print("**** Created list of following ****", flush=True)
             bastards = [user for user in following if user not in followers]
             return bastards
             
         def unfollow_bastards(self, n):
             mofos = self.get_unfollowers()
-            print("**** Created list of bastards who didn´t follow you back ****")
+            print("**** Created list of bastards who didn´t follow you back ****", flush=True)
             self._human_sleep(4)
             self.driver.find_element_by_xpath("//a[contains(@href, 'following')]")\
                 .click()
@@ -63,13 +63,11 @@ class InstaBot:
                 
                 self._human_sleep(5)
                 button.click()
+                print("**** Stopped following " + mofos[m-n] + " ****")
                 self._human_sleep(2)
 
                 self.driver.find_element_by_xpath("//button[contains(text(), 'Unfollow')]")\
                     .click()
-                    #print("**** That was a public user ****")
-                
-                sleep(60*60)
                 
                 self._human_sleep_slow(3)
 
@@ -112,14 +110,36 @@ class InstaBot:
             sleep(n + randint(0,1))
         def _take_naps(self, n):
             if n == 5:
-                print("**** Napping 5 min ****")
-                sleep(60*5)
+                print("**** Napping 5 min ****", flush=True)
+                sleep(63*5)
             if n == 12:
-                print("**** napping 11 min ****")
+                print("**** napping 11 min ****", flush=True)
                 sleep(60*11)
-            
+            if n == 20:
+                print("**** napping 14 min ****", flush=True)
+                sleep(52*14)
+            if n == 24:
+                print("**** napping 3 min ****", flush=True)
+                sleep(55*3)
+            if n == 29:
+                print("**** napping 4 min ****", flush=True)
+                sleep(64*4)
+            if n == 31:
+                print("**** napping 23 min ****", flush=True)
+                sleep(60*23)
+            if n == 37:
+                print("**** napping 6 min ****", flush=True)
+                sleep(59*6)
+            if n == 42:
+                print("**** napping 5 min ****", flush=True)
+                sleep(49*5)
+            if n == 46:
+                print("**** napping 9 min ****", flush=True)
+                sleep(57*9)
+            if n == 49:
+                print("**** Napping 5 min ****", flush=True)
+                sleep(56*5)
             
                      
 a_bot = InstaBot('account', 'pass')
-a_bot.unfollow_bastards(15)
-
+a_bot.unfollow_bastards(50)
