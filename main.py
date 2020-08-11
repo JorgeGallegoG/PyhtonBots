f1af652ec7e8d08b48009b5f56973df727a57aa1
JorgeGallegoG <jorgallegog@gmail.com>
2020-08-11 09:42:17 +0200

    make list of unfollowers global so we can reuse it in a second unfollow sequence

--- main.py
+++ main.py
@@ -31,6 +31,7 @@ class InstaBot:
                 .click()
         _following_path = ".\data"
         _following_file_name = "following"
+        mofos = None
                 
         def get_unfollowers(self):
             self.driver.find_element_by_xpath("//a[contains(@href,'/{}')]".format(self.username))\
@@ -48,26 +49,29 @@ class InstaBot:
             return bastards
             
         def unfollow_bastards(self, n):
-            mofos = self.get_unfollowers()
-            print("**** Created list of bastards who didn´t follow you back ****", flush=True)
+            #Check if is the first execution (otherwise we have the list generated)
+            if self.mofos == None:
+                self.mofos = self.get_unfollowers()
+                print("**** Created list of bastards who didn´t follow you back ****", flush=True)
             self._human_sleep(4)
             self.driver.find_element_by_xpath("//a[contains(@href, 'following')]")\
                 .click()
             self._human_sleep(2)
             scroll_box = self.driver.find_element_by_xpath("/html/body/div[4]/div/div/div[2]")
             self._load_all_contacts(scroll_box)
+            self._human_sleep(10*61)
             m = n
             while n > 0:
                 self._take_naps(n)
                 self._human_sleep(2)
                 
-                name_element = self.driver.find_element_by_xpath("//a[contains(@href, '/{}')]".format(mofos[m-n]))
+                name_element = self.driver.find_element_by_xpath("//a[contains(@href, '/{}')]".format(self.mofos[m-n]))
                 antecesor = self._find_ancestor(5, name_element) 
                 button = antecesor.find_element_by_xpath(".//button[contains(text(), 'Following')]")
 
                 self._human_sleep(5)
                 button.click()
-                print("**** Stopped following " + mofos[m-n] + " ****")
+                print("**** Stopped following " + self.mofos[m-n] + " ****")
                 self._human_sleep(2)
 
                 self.driver.find_element_by_xpath("//button[contains(text(), 'Unfollow')]")\
@@ -92,8 +96,10 @@ class InstaBot:
                 #Start following people
                 followed_list = []
                 scroll_box = self.driver.find_element_by_xpath("/html/body/div[4]/div/div/div[2]")
-                changed_scroll_flag = False 
-                for x in range(n_of_fans):
+                changed_scroll_flag = False
+                x = 0
+                while x < n_of_fans:
+                #for x in range(n_of_fans):
                     self._take_naps(x)
                     #When we are accesing this manu from the "previous page function" the xpath of the scroll box is changed
                     if changed_scroll_flag == True: 
@@ -101,7 +107,7 @@ class InstaBot:
                     self._human_sleep(2)
                     flag = False
                     while flag == False:
-                        self._human_sleep(5)
+                        self._human_sleep(4)
                         try:
                             button_to_follow = scroll_box.find_element_by_xpath(".//button[text()='Follow']")
                             flag = True
@@ -118,10 +124,10 @@ class InstaBot:
                                                        arguments[0].scrollTo(0, arguments[0].scrollHeight);
                                                        return arguments[0].scrollHeight;
                                                    """, scroll_box)
-                            self._human_sleep(4)
+                            self._human_sleep(3)
                             continue
                     button_to_follow.click()
-                    self._human_sleep(5)
+                    self._human_sleep(3)
                     antecesor = self._find_ancestor(3, button_to_follow)
                     list_name_of_followed_b = antecesor.find_elements_by_tag_name('a')
                     temp_list_names_of_followed_text = [name.text for name in list_name_of_followed_b if name.text != '']
@@ -131,10 +137,11 @@ class InstaBot:
                         name_of_followed.click()
                         self._human_sleep(3)
                         print(temp_list_names_of_followed_text[0])
+                        x =+ 1
                         followed_list.extend(temp_list_names_of_followed_text)
                         self.driver.find_element_by_xpath("//button[contains(text(), 'Message')]")\
                         .click()
-                        self._human_sleep(4)
+                        self._human_sleep(3)
                         
                         #When we are accesing this manu from the "previous page function" the xpath of the scroll box is changed
                         changed_scroll_flag = True
@@ -145,28 +152,29 @@ class InstaBot:
                             self._send_messages()
                             self._human_sleep_fast(2)
                         self.driver.execute_script("window.history.go(-1)")
-                        self._human_sleep(4)
+                        self._human_sleep(3)
                         self.driver.execute_script("window.history.go(-1)")
                     except NoSuchElementException:
                         pass
                         
                     self._human_sleep(1)
                     temp_list_names_of_followed_text = []
-                    self._human_sleep(4)
+                    self._human_sleep(3)
                     
                 with open (self._following_path + self._following_file_name + ".csv",'a') as following_file:
                     writer = csv.writer(following_file, dialect='excel')
                     writer.writerow(followed_list)
-                    self._human_sleep(4)
-                    self.driver.execute_script("window.history.go(-1)")
-                    self._human_sleep(5)
-                    self.driver.execute_script("window.history.go(-1)")
                     self._human_sleep(3)
+                    self.driver.execute_script("window.history.go(-1)")
+                    self._human_sleep(1)
+                    self.driver.execute_script("window.history.go(-1)")
+                    self._human_sleep(1)
                     self.driver.execute_script("window.history.go(-1)")
                
         def _send_messages(self):
             list_messages = self._generate_messages()
-            self._human_sleep(6)
+            self._human_sleep(5)
+            print("**** Sending Messages ****")
             for msg in list_messages:
                 message_bar = self.driver.find_element_by_xpath("//textarea[@placeholder=\"Message...\"]")
                 #Using javascript to send emojis (not suported by send kesys method)
@@ -188,11 +196,12 @@ class InstaBot:
             
         #Method to generate a list of messages that when put toguether generate a conversation
         def _generate_messages(self):
+            print("**** Setting messages ****")
             all_messages = []
             
             #Every list contains all the possible messages in that possition (inside de message secuence)
             all_messages.append(["Hi human!", "Hey human!", "I like your style human!"])
-            all_messages.append (["I am a monster 😊, But a good one ☝"])
+            #all_messages.append (["I am a monster 😊, But a good one ☝"])
             all_messages.append (["I came from the Monsterland ", "I came to the Humanland "])
             all_messages.append (["to show humans the sounds we monsters do", "to show humans the sounds of monsters", "to show you the sounds of monsters", "to show you the sounds we monsters do"])
             all_messages.append (["Do you like DnB?, check my track!", "If you like trippy stuff, check out:", "I released this song with some cool animations 👇"])
@@ -245,38 +254,26 @@ class InstaBot:
         def _human_sleep_fast(self, n):
             sleep(n + randint(0,1))
         def _take_naps(self, n):
-            if n == 4:
-                print("**** Napping 5 min ****", flush=True)
-                sleep(63*2)
             if n == 10:
-                print("**** napping 11 min ****", flush=True)
-                sleep(60*5)
-            if n == 17:
-                print("**** napping 14 min ****", flush=True)
-                sleep(52*7)
-            if n == 25:
                 print("**** napping 3 min ****", flush=True)
-                sleep(55*3)
-            if n == 30:
-                print("**** napping 4 min ****", flush=True)
-                sleep(64*2)
-            if n == 33:
-                print("**** napping 23 min ****", flush=True)
-                sleep(60*14)
-            if n == 37:
-                print("**** napping 6 min ****", flush=True)
-                sleep(59*4)
-            if n == 40:
+                sleep(60*13)
+            if n == 17:
                 print("**** napping 5 min ****", flush=True)
-                sleep(49*3)
-            if n == 45:
+                sleep(52*15)
+            if n == 25:
+                print("**** napping 1 min ****", flush=True)
+                sleep(55*11)
+            if n == 33:
                 print("**** napping 9 min ****", flush=True)
-                sleep(57*4)
+                sleep(60*19)
+            if n == 40:
+                print("**** napping 1 min ****", flush=True)
+                sleep(49*11)
             if n == 48:
-                print("**** Napping 5 min ****", flush=True)
-                sleep(56*2)
+                print("**** Napping 2 min ****", flush=True)
+                sleep(56*12)
                             
 a_bot = InstaBot('account', 'pass')
-#a_bot.unfollow_bastards(50)
-a_bot.talk_to_fans_of("account_to_follow_name", 45)
+a_bot.unfollow_bastards(55)
+#a_bot.talk_to_fans_of("account_to_follow", 60)
 #print(a_bot._generate_messages())
